95c54126ea86d57395128b01f7badf7234d8d563
1053910251 <1053910251@qq.com>
2018-08-15 21:58:03 +0800

    修改视频大小限制

--- src/components/Upload/video.vue
+++ src/components/Upload/video.vue
@@ -50,13 +50,13 @@
         this.emitSuccess(res)
       },
       beforeUpload(file) {
-        const isLt10M = file.size / (1024 * 1024) < 10
+        const isLt10M = file.size / (1024 * 1024) < 100
         if (['video/mp4', 'video/ogg', 'video/flv', 'video/avi', 'video/wmv', 'video/rmvb'].indexOf(file.type) === -1) {
           this.$message.error('请上传正确的视频格式')
           return false
         }
         if (!isLt10M) {
-          this.$message.error('上传视频大小不能超过10MB哦!')
+          this.$message.error('上传视频大小不能超过100MB哦!')
           return false
         }
       },
